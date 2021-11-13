b473dd5bebb861c87d49367fb5039a63a3dccb3c
dyiqndhad <90169507+dyiqndhad@users.noreply.github.com>
2021-11-13 00:32:34 +0000

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 # configs
-mkdir -p /etc/caddy/ /usr/share/caddy
-cat > /usr/share/caddy/robots.txt << EOF
-User-agent: *
-Disallow: /
-EOF
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+wget -qO- $CONFIGV2RAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/v2ray.json
+
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
 
 for file in $(ls /usr/share/caddy/$AUUID); do
     [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
 done
 
-chmod +x ./v2ray ./v2ctl
-
 # start
-tor & /usr/local/bin/v2ray -config /xray.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+tor &
+
+/v2ray -config /v2ray.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
